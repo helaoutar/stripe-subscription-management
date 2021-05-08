90599095dc4c21cf20059a865b4d21fe757b7683
ESElAoutHa <elaoutarhamza@gmail.com>
2021-05-08 23:43:58 +0000

    cmon now

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify({
       app_metadata: {
-        roles: ["sub:free"],
+        roles: ["free"],
       },
     }),
   };
